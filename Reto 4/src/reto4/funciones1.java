d5aeb49fb2b76284a50d5a7d9558857428aa3ffd
alvarezleiva <galvarezleiv@gmail.com>
2024-11-19 12:20:22 +0100

    fix

--- Reto 4/src/reto4/funciones1.java
+++ Reto 4/src/reto4/funciones1.java
@@ -1,5 +1,17 @@
 package reto4;
 
 public class funciones1 {
- 
+	public static void dimelo(int producto) {// hace una piramide
+
+		for (int i = 0; i < producto; i++) {
+			for (int j = i; j <= i; j++) {
+				System.out.print(" ");
+			}
+			for (int p = 1; p <= i; p--) {
+				System.out.print("*");
+
+			}
+			System.out.println();
+		}
+	}
 }
