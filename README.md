8716da34a29d306a9a572d8ee38a8aff44a03937
alvarezleiva <galvarezleiv@gmail.com>
2024-11-19 12:20:14 +0100

    arreglar main

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< Upstream, based on branch 'main' of https://github.com/alvarezleiva/Reto4Corregido.git
 # Reto4Corregido
+=======
+# reto4
+>>>>>>> e384a8b Initial commit
--- Reto 4/src/reto4/reto4.java
+++ Reto 4/src/reto4/reto4.java
@@ -1,6 +1,25 @@
 package reto4;
 
+import java.util.Scanner;
+
 public class reto4 {
+	// este main calcula los multiplos de un n�mero y luego con
+	// la funcion hace una piramide con ese numero de estrellas
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		int i = 0;
+		int producto = 0;
+		// Aqui pide un n�mero
+		funciones.dimeEntero("Introduce un numero", sc);
+	
+			for (i = 1; i < num; i++)
+			{
+				if(num % i == 0)
+				producto = num * i;
+			}
+		funciones1.dimelo(producto);
+	}
 
 }
- 
